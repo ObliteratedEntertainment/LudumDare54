a743f673a53138b9c82222a2aa307d7a102d8b90
Rex <imaperson@users.noreply.github.com>
2023-09-30 12:49:19 -0400

    7 setup character foundation (#8)
    * Character foundation
    
    * Give holy quest spaces

---
+++ characters/base/character.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@onready var neutral_sprite = $Neutral
+@onready var success_sprite = $Success
+@onready var failure_sprite = $Failure
+@onready var quest = $Quest
+@onready var lore = $Lore
